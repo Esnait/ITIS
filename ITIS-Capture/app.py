bc85d8a74f487c9b60658e95f07f2321e38836d6
Esnait <96617742+Esnait@users.noreply.github.com>
2023-04-30 17:31:43 +0530

    initial commit #2

--- ITIS-Capture/app.py
+++ ITIS-Capture/app.py
@@ -1,19 +1,12 @@
 from flask import Flask, request
 import io
-from predictor import MODEL, load_trained_model, capture_status
+from predictor import capture_status
 
 app = Flask(__name__)
-
-
-@app.before_first_request
-def my_func():
-    load_trained_model()
-
 
 @app.route('/', methods=['GET'])
 def test_connection():
     return "Success"
-
 
 @app.route('/predict', methods=['POST'])
 def capture():
--- ITIS-Capture/predictor.py
+++ ITIS-Capture/predictor.py
@@ -1,30 +1,29 @@
-from keras.models import load_model
-from keras.preprocessing import image
-from PIL import Image
-import base64
-import io
+import PIL
+import tensorflow as tf
 import numpy as np
 
-MODEL = {}
+TF_MODEL_FILE_PATH = 'resources/model.tflite'
+class_names = ['F', 'N', 'Q', 'S', 'V']
+label = {
+    "N" : "Normal",
+    "S" : "Supraventricular ectopic",
+    "V" : "Ventricular ectopic",
+    "F" : "Fusion",
+    "Q" : "Unknown"
+}
 
-index = ['Left Bundle Branch block',
-         'Normal',
-         'Premature Atrial Contraction',
-         'Premature Ventricular Contraction',
-         'Right Bundle Branch Block',
-         'Ventricular Fibrillation']
-
-
-def load_trained_model():
-    MODEL['trained'] = load_model(r'resources/model/ECG.h5')
-
+interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
+classify_lite = interpreter.get_signature_runner('serving_default')
 
 def capture_status(ecg_image_stream):
-    img = image.image_utils.load_img(ecg_image_stream, target_size=(64, 64))
-    x = image.image_utils.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
+    img = tf.keras.utils.load_img(ecg_image_stream, target_size=(256, 256))
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
 
-    pred = MODEL['trained'].predict(x)
-    y_pred = np.argmax(pred)
+    predictions_lite = classify_lite(sequential_1_input=img_array)['outputs']
+    score_lite = tf.nn.softmax(predictions_lite)
 
-    return str(index[y_pred])
+    print("Prediction : '{}' with a {:.2f} percent confidence."
+    .format(label[class_names[np.argmax(score_lite)]], 100 * np.max(score_lite)))
+
+    return label[class_names[np.argmax(score_lite)]]
